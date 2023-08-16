a345b67dd7443643738a5ce2e8db74ce444c39df
Heyner Urbina <heynerzepeda@hotmail.com>
2023-08-16 17:57:05 -0600

    Primer commit

---
+++ ProyectoBiblioteca/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBiblioteca.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=DESKTOP-9PSM3I9\\SQLEXPRESS;Initial Catalog=DB_BIBLIOTECA;Integrated Security=True";
+    }
+}
